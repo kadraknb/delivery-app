bfb9db7c38a0bada3743d2b1a4485af93ccd783a
Matthews Bastos <matthewsbasttos@gmail.com>
2023-03-01 16:49:38 -0300

    feat: Rota get de products pronta para uso

--- back-end/src/Routes/index.js
+++ back-end/src/Routes/index.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const UserRouter = require('./UserRouter');
+const ProductRouter = require('./ProductRouter');
 
 const router = Router();
 
 router.use(UserRouter);
+router.use(ProductRouter);
 
 module.exports = router;
