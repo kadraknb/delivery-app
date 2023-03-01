05ad6dd1f812722f35998c3b24ba53bacbe54b97
xWesleyMartins <wesley_869@hotmail.com.br>
2023-03-01 18:19:54 -0300

    feat: adiciona validação de createUser

--- back-end/src/utils/validate/userValidate.js
+++ back-end/src/utils/validate/userValidate.js
@@ -1,7 +1,6 @@
 const md5 = require('md5');
 const SchemaJoi = require('./schemaJoi');
-const NotFound = require('../errors/NotFound');
-const BadRequest = require('../errors/BadRequest');
+const { NotFound, BadRequest, Conflict } = require('../errors');
 
 module.exports = class Validate {
   static verifyLogin(email, password, verifyUserExist) {
@@ -17,6 +16,19 @@ module.exports = class Validate {
     const hashMD5 = md5(password);
     if (verifyUserExist.password !== hashMD5) {
       throw new BadRequest('incorrect password');
+    }
+  }
+
+  static validCreateUser(verifyUserExist, user) {
+    const { error } = SchemaJoi.createUserSchema.validate(user);
+    console.log(error.details);
+    if (error) {
+      throw new BadRequest(error.details[0].message);
+    }
+
+    const { email, name } = verifyUserExist;
+    if (email === user.email || name === user.name) {
+      throw new Conflict('user exist');
     }
   }
 };
