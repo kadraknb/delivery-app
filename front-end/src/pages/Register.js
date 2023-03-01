260e5c007f426168e1510e4d26be519815666433
Vinicius Kaminski <vikaminski@hotmail.com>
2023-03-01 19:08:52 -0300

    fix: lint styles css

--- front-end/src/pages/Register.js
+++ front-end/src/pages/Register.js
@@ -2,7 +2,6 @@ import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import DeliveryContext from '../context/DeliveryContext';
 import api from '../services/api';
-import './form.css';
 
 function Register() {
   const { email, password, setEmail, setPassword } = useContext(DeliveryContext);
