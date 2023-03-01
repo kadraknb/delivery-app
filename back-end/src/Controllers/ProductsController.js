2d1b176231486606dc48bbd833df5d4ef8f4cf17
Matthews Bastos <matthewsbasttos@gmail.com>
2023-03-01 16:44:27 -0300

    feat: Cria o método getAllProducts da classe ProductsController

--- back-end/src/Controllers/ProductsController.js
+++ back-end/src/Controllers/ProductsController.js
@@ -2,4 +2,13 @@ module.export = class ProductsController {
   constructor(service) {
     this.service = service;
   }
+
+  async getAllProducts(_req, res, next) {
+    try {
+      const allProducts = await this.service.getAllProducts();
+      return res.status(200).json(allProducts);
+    } catch (error) {
+      next(error);
+    }
+  }
 };
