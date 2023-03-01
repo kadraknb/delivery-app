3f6d1fbad70708c5a81a220cd6040ad3408523c1
Vinicius Soares <viniciussoares72943@gmail.com>
2023-03-01 08:44:20 -0300

    feat: Cria endpoint /login

--- back-end/src/Services/UserService.js
+++ back-end/src/Services/UserService.js
@@ -1,5 +1,17 @@
-module.export = class UserService {
-   constructor(model) {
+const validate = require('../utils/validate/userValidate');
+
+module.exports = class UserService {
+  constructor(model) {
     this.model = model;
+  }
+
+  async getUserEmail(email) {
+    const user = await this.model.findOne({ where: { email } });
+    return user;
+  }
+
+  async login(email, password) {
+    const verifyUserExist = await this.getUserEmail(email);
+    validate.verifyLogin(email, password, verifyUserExist);
   }
 };
