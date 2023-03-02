c50ae74d5a44b798e218573e7898c413786ecdff
kadraknb <kadrak02@hotmail.com>
2023-03-02 17:36:23 -0300

    refactor: removendo o id do token

--- back-end/src/utils/auth/TokenGenerator.js
+++ back-end/src/utils/auth/TokenGenerator.js
@@ -3,7 +3,6 @@ const jwt = require('jsonwebtoken');
 module.exports = class TokenGenerator {
   static generateToken(user) {
     const payload = {
-      id: user.id,
       name: user.name,
       email: user.email,
     };
