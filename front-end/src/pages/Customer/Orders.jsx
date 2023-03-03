a1dcf0cc8606e5d4065a1c89c93ca34b18d55284
Matthews Bastos <matthewsbasttos@gmail.com>
2023-03-03 17:06:46 -0300

    feat: Cria uma função para pegar todos os pedidos do usuario

--- front-end/src/pages/Customer/Orders.jsx
+++ front-end/src/pages/Customer/Orders.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import NavBar from '../../components/NavBar';
+import api from '../../services/axios';
 
 function Orders() {
+  const [salesData, setSalesData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getAllSales = async () => {
+    try {
+      const { data } = await api.get('/customer/orders');
+
+      setSalesData(data);
+      setIsLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getAllSales();
+  }, []);
+
   return (
     <div>
       <NavBar />
