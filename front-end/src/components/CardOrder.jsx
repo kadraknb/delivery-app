c3bc13bf43575fe7e1a8fffa4a4f6cf4aa2115cd
Matthews Bastos <matthewsbasttos@gmail.com>
2023-03-03 17:18:10 -0300

    feat: Agora o card do pedido é um botão que redireciona para a página de detalhes

--- front-end/src/components/CardOrder.jsx
+++ front-end/src/components/CardOrder.jsx
@@ -4,29 +4,34 @@ import PropTypes from 'prop-types';
 function CardOrder({ id, status, saleDate, totalPrice }) {
   return (
     <div>
-      <p
-        data-testid={ `customer_orders__element-order-id-${id}` }
+      <button
+        type="button"
+        onClick={ () => navigate(`/customer/orders/${id}`) }
       >
-        Pedido
-        { id }
-      </p>
-      <p
-        data-testid={ `customer_orders__element-delivery-status-${id}` }
-      >
-        { status }
-      </p>
-      <div>
         <p
-          data-testid={ `customer_orders__element-order-date-${id}` }
+          data-testid={ `customer_orders__element-order-id-${id}` }
         >
-          { saleDate }
+          Pedido
+          { id }
         </p>
         <p
-          data-testid={ `customer_orders__element-card-price-${id}` }
+          data-testid={ `customer_orders__element-delivery-status-${id}` }
         >
-          { totalPrice }
+          { status }
         </p>
-      </div>
+        <div>
+          <p
+            data-testid={ `customer_orders__element-order-date-${id}` }
+          >
+            { saleDate }
+          </p>
+          <p
+            data-testid={ `customer_orders__element-card-price-${id}` }
+          >
+            { totalPrice }
+          </p>
+        </div>
+      </button>
     </div>
   );
 }
