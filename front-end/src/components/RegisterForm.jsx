2df4be4abb3e52d1f58408cff5e9cce8eeceac7c
Matthews Bastos <matthewsbasttos@gmail.com>
2023-03-06 16:40:29 -0300

    feat: Cria o formulário da tela Manage

---
+++ front-end/src/components/RegisterForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function RegisterForm() {
+  return (
+    <div>
+      Register Form
+    </div>
+  );
+}
+
+export default RegisterForm;
--- front-end/src/pages/Admin/Manage.js
+++ front-end/src/pages/Admin/Manage.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import NavBar from '../../components/NavBar';
+import RegisterForm from '../../components/RegisterForm';
 
 function Manage() {
   return (
     <div>
       <NavBar />
-      Manage
+      <RegisterForm />
     </div>
   );
 }
