b6ec402aa61140b9cf104b67847753beb710845d
Matthews Bastos <matthewsbasttos@gmail.com>
2023-03-06 16:21:04 -0300

    feat: Cria a página Admin

---
+++ front-end/src/pages/Admin/Manage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import NavBar from '../../components/NavBar';
+
+function Manage() {
+  return (
+    <div>
+      <NavBar />
+      Manage
+    </div>
+  );
+}
+
+export default Manage;
