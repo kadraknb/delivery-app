3c98579fab7fbede6dd3a6be1f59e8b0f2b43981
Matthews Bastos <matthewsbasttos@gmail.com>
2023-03-06 16:51:55 -0300

    feat: Adiciona todos os elementos do formulário de criação de usuário

--- front-end/src/components/RegisterForm.jsx
+++ front-end/src/components/RegisterForm.jsx
@@ -3,7 +3,49 @@ import React from 'react';
 function RegisterForm() {
   return (
     <div>
-      Register Form
+      <h2>Cadastrar novo usuário</h2>
+      <form>
+        <label htmlFor="input-email">
+          Nome
+          <input
+            type="text"
+            onChange={ () => '' }
+            data-testid="admin_manage__input-name"
+          />
+        </label>
+        <label htmlFor="input-email">
+          Email
+          <input
+            type="email"
+            onChange={ () => '' }
+            data-testid="admin_manage__input-email"
+          />
+        </label>
+        <label htmlFor="label-password">
+          Password
+          <input
+            type="password"
+            onChange={ () => '' }
+            data-testid="admin_manage__input-password"
+          />
+        </label>
+        <label htmlFor="label-type">
+          Tipo
+          <select
+            data-testid="admin_manage__select-role"
+          >
+            <option>Vendedor</option>
+            <option>Cliente</option>
+            <option>Administrador</option>
+          </select>
+        </label>
+        <button
+          type="submit"
+          data-testid="common_register__button-register"
+        >
+          Cadastrar
+        </button>
+      </form>
     </div>
   );
 }
