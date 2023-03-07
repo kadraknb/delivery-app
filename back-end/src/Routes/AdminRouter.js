583052fe73565fa71fb34e5f50bce2f65e997096
Matthews Bastos <matthewsbasttos@gmail.com>
2023-03-07 17:50:55 -0300

    fix: Lint

--- back-end/src/Routes/AdminRouter.js
+++ back-end/src/Routes/AdminRouter.js
@@ -7,7 +7,7 @@ const AdminRouter = Router();
 AdminRouter.post(
   '/admin/manage',
   TokenGenerator.validateToken,
-  (req, res, next) => UserFactory.createUser(req, res, next)
+  (req, res, next) => UserFactory.createUser(req, res, next),
 );
 
 module.exports = AdminRouter;
