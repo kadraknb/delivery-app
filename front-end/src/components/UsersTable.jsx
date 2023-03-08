72ef2b9f0a9c0efa6862a3c089301ed43c96bb44
Matthews Bastos <matthewsbasttos@gmail.com>
2023-03-08 16:34:27 -0300

    feat: Cria a tabela de usuarios

---
+++ front-end/src/components/UsersTable.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function UsersTable() {
+  return (
+    <table>
+      <div>Table</div>
+    </table>
+  );
+}
+
+export default UsersTable;
--- front-end/src/pages/Admin/Manage.js
+++ front-end/src/pages/Admin/Manage.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import NavBar from '../../components/NavBar';
 import RegisterForm from '../../components/RegisterForm';
+import UsersTable from '../../components/UsersTable';
 
 function Manage() {
   return (
     <div>
       <NavBar />
       <RegisterForm />
+      <UsersTable />
     </div>
   );
 }
