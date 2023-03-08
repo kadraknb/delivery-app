a20594bcc0537cc957d17f1e1c0bdda17524e2dd
Matthews Bastos <matthewsbasttos@gmail.com>
2023-03-08 16:41:09 -0300

    feat: Adiciona todos os elementos da tabela

--- front-end/src/components/UsersTable.jsx
+++ front-end/src/components/UsersTable.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function UsersTable() {
+function UsersTable({ itemNumber, name, email, role }) {
   return (
     <table>
-      <div>Table</div>
+      <tr>
+        <th>Item</th>
+        <th>Nome</th>
+        <th>E-mail</th>
+        <th>Tipo</th>
+        <th>Excluir</th>
+      </tr>
+      <tr>
+        <td>{ itemNumber }</td>
+        <td>{ name }</td>
+        <td>{ email }</td>
+        <td>{ role }</td>
+        <td>
+          <button
+            type="button"
+            onClick=""
+          >
+            Excluir
+          </button>
+        </td>
+      </tr>
     </table>
   );
 }
+
+UsersTable.propTypes = {
+  itemNumber: PropTypes.number,
+  name: PropTypes.string,
+  email: PropTypes.string,
+  role: PropTypes.string,
+}.isRequired;
 
 export default UsersTable;
