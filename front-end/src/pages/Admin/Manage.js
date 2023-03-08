547a4b4428b38d0fc0989796226e57a0a353d765
Matthews Bastos <matthewsbasttos@gmail.com>
2023-03-08 17:28:23 -0300

    feat: Cria uma função para pegar todos os usuários

--- front-end/src/pages/Admin/Manage.js
+++ front-end/src/pages/Admin/Manage.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import NavBar from '../../components/NavBar';
 import RegisterForm from '../../components/RegisterForm';
 import UsersTable from '../../components/UsersTable';
+import api from '../../services/api';
 
 function Manage() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getAllUsers = async () => {
+    try {
+      const { data } = await api.get('admin/manage');
+
+      setUsers(data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <NavBar />
