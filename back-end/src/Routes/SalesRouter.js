b8563bec5c34caec599cfb4110b58b79c1bbf6b7
Vinicius Soares <viniciussoares72943@gmail.com>
2023-03-09 09:29:09 -0300

    fix: nome da função estava errada

--- back-end/src/Routes/SalesRouter.js
+++ back-end/src/Routes/SalesRouter.js
@@ -1,22 +1,15 @@
-const { Router } = require("express");
-// const SalesController = require('../Controllers/SalesController');
-const SalesFactory = require("../Factory/SalesFactory");
-// const validateToken = require('../middlewares/validateToken');
-const TokenGenerator = require("../utils/auth/TokenGenerator");
+const { Router } = require('express');
+const SalesFactory = require('../Factory/SalesFactory');
+
 const SalesRouter = Router();
 
-SalesRouter.get("/customer/orders", (req, res, next) =>
-  SalesFactory.getAllSales(req, res, next)
-);
+SalesRouter.get('/customer/orders/:id', (req, res, next) =>
+  SalesFactory.getAllSalesbyUserId(req, res, next));
 
-// SalesRouter.use(TokenGenerator.validateToken)
+SalesRouter.post('/sales', (req, res, next) =>
+  SalesFactory.createSales(req, res, next));
 
-SalesRouter.post("/sales", (req, res, next) =>
-  SalesFactory.createSales(req, res, next)
-);
-
-SalesRouter.put("/sales/detail/:id", (req, res, next) =>
-  SalesFactory.changeStateOfSaleById(req, res, next)
-);
+SalesRouter.put('/sales/detail/:id', (req, res, next) =>
+  SalesFactory.changeStateOfSaleById(req, res, next));
 
 module.exports = SalesRouter;
