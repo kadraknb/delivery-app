b5be764b3077762df31d42656f4f57ae784395ca
Vinicius Soares <viniciussoares72943@gmail.com>
2023-03-09 14:54:40 -0300

    fix: arruma pagina de detalhes de venda

--- front-end/src/pages/seller/saleOrder.jsx
+++ front-end/src/pages/seller/saleOrder.jsx
@@ -3,18 +3,18 @@ import React, { useEffect, useState } from 'react';
 import { getSaleDetail, changeStateApiOrders } from '../../services/apiOrders';
 import TableProducts from '../../components/TableProducts';
 import NavBar from '../../components/NavBar';
-import LocalStorage from '../../utils/localStorage.utils';
 
 function PersonSallerOrdersDetail() {
   const [seleOrders, setSeleOrders] = useState({});
   const [arrProducts, setArrProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const id = Number(global.location.pathname.replace('/seller/orders/', ''));
-  const token = LocalStorage.localGetToken();
 
   const saveSaleDetail = async () => {
-    const orders = await getSaleDetail(id, token);
-    setArrProducts(orders.itSold.map((obj) => ({ ...obj, ...obj.products })));
+    const orders = await getSaleDetail(id);
+    setArrProducts(orders);
     setSeleOrders(orders);
+    setIsLoading(false);
   };
 
   const changeState = async (newStatus) => {
@@ -26,59 +26,63 @@ function PersonSallerOrdersDetail() {
     saveSaleDetail();
   }, []);
 
-  console.log('🚀 ~ PersonSallerOrdersDetail ~ seleOrders', seleOrders);
+  const dataName = 'seller_order_details__element-order-details-label-delivery-status';
   return (
     <div>
       <NavBar />
-      <h3>Detalhe do Pedido</h3>
-      <span>
-        <p data-testid="seller_order_details__element-order-details-label-order-id">
-          PEDIDO
-          {' '}
-          {seleOrders.id}
-        </p>
-        <p data-testid="seller_order_details__element-order-details-label-order-date">
-          {new Date(seleOrders.saleDate).toLocaleDateString('pt-br')}
-        </p>
-        <p
-          data-testid="seller_order_details__element-order-details-label-delivery-status"
-        >
-          {seleOrders.status}
-        </p>
-        <button
-          data-testid="seller_order_details__button-preparing-check"
-          type="button"
-          onClick={ () => changeState('Preparando') }
-          disabled={ seleOrders.status !== 'Pendente' }
-        >
-          PREPARAR PEDIDO
-        </button>
-        <button
-          data-testid="seller_order_details__button-dispatch-check"
-          type="button"
-          onClick={ () => changeState('Em Trânsito') }
-          disabled={ seleOrders.status !== 'Preparando' }
-        >
-          Pendente
-        </button>
-      </span>
-      <table>
-        <thead>
-          <tr>
-            <th>Item</th>
-            <th>Description</th>
-            <th>Quantity</th>
-            <th>Unitary Price</th>
-            <th>Sub-Total</th>
-          </tr>
-        </thead>
-        <tbody>
-          <TableProducts array={ arrProducts } />
-        </tbody>
-      </table>
-      <h1 data-testid="seller_order_details__element-order-total-price">
-        {Number(seleOrders.totalPrice).toFixed(2).replace('.', ',')}
-      </h1>
+      {!isLoading && (
+        <div>
+          <h3>Detalhe do Pedido</h3>
+          <span>
+            <p data-testid="seller_order_details__element-order-details-label-order-id">
+              PEDIDO
+              {' '}
+              {seleOrders.id}
+            </p>
+            <p data-testid="seller_order_details__element-order-details-label-order-date">
+              {new Date(seleOrders.saleDate).toLocaleDateString('pt-br')}
+            </p>
+            <p
+              data-testid={ dataName }
+            >
+              {seleOrders.status}
+            </p>
+            <button
+              data-testid="seller_order_details__button-preparing-check"
+              type="button"
+              onClick={ () => changeState('Preparando') }
+              disabled={ seleOrders.status !== 'Pendente' }
+            >
+              PREPARAR PEDIDO
+            </button>
+            <button
+              data-testid="seller_order_details__button-dispatch-check"
+              type="button"
+              onClick={ () => changeState('Em Trânsito') }
+              disabled={ seleOrders.status !== 'Preparando' }
+            >
+              Pendente
+            </button>
+          </span>
+          <table>
+            <thead>
+              <tr>
+                <th>Item</th>
+                <th>Description</th>
+                <th>Quantity</th>
+                <th>Unitary Price</th>
+                <th>Sub-Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              <TableProducts array={ arrProducts } />
+            </tbody>
+          </table>
+          <h1 data-testid="seller_order_details__element-order-total-price">
+            {Number(seleOrders.totalPrice).toFixed(2).replace('.', ',')}
+          </h1>
+        </div>
+      )}
     </div>
   );
 }
