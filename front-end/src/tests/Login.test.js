70478caf122baa1238f4e001c1653937b9c4b376
Matthews Bastos <matthewsbasttos@gmail.com>
2023-03-10 17:19:32 -0300

    test: Cria o caso de teste de sucesso para a tela de login

---
+++ front-end/src/tests/Login.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './helpers/renderWithRouter';
+
+describe('Testa a página de Login', () => {
+  it('Deve realizar o login com sucesso', () => {
+    
+  });
+});
