b8c190877e34623d426d1755bf92421b78fc0b89
Matthews Bastos <matthewsbasttos@gmail.com>
2023-03-10 17:50:57 -0300

    test: Teste da página de login

--- front-end/src/tests/Login.test.js
+++ front-end/src/tests/Login.test.js
@@ -6,6 +6,20 @@ import renderWithRouter from './helpers/renderWithRouter';
 
 describe('Testa a página de Login', () => {
   it('Deve realizar o login com sucesso', () => {
-    
+    renderWithRouter(<App />);
+
+    const loginBtn = screen.getByTestId('common_login__button-login');
+    expect(loginBtn).toBeDisabled();
+
+    const emailInput = screen.getByTestId('common_login__input-email');
+    userEvent.type(emailInput, 'zebirita@email.com');
+    expect(emailInput).toHaveProperty('value', 'zebirita@email.com');
+
+    const passwordInput = screen.getByTestId('common_login__input-password');
+    userEvent.type(passwordInput, '$#zebirita#$');
+    expect(passwordInput).toHaveProperty('value', '$#zebirita#$');
+
+    expect(loginBtn).not.toBeDisabled();
+    userEvent.click(loginBtn);
   });
 });
