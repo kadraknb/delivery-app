59448ed44d2942c2380f7b5ccd6218d194ed9f36
kadraknb <kadrak02@hotmail.com>
2023-06-02 16:49:22 -0300

    fix: ajusto o DB para o docker

--- app/backend/src/database/config/config.js
+++ app/backend/src/database/config/config.js
@@ -1,22 +1,11 @@
 require('dotenv').config();
 
-const environment = process.env.NODE_ENV || "test";
-
-const suffix = {
-  prod: "",
-  production: "",
-  dev: "-dev",
-  development: "-dev",
-  test: "-test",
-};
-
 const options = {
-  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || '3306',
-  database: 
-    `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'password',
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || '3002',
+  username: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || '123456',
+  database: process.env.DB_NAME || 'delivery-app',
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
