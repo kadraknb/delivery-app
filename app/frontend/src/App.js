458423a638158854dbdbd109d47911806036c10d
kadraknb <kadrak02@hotmail.com>
2023-06-21 04:02:37 -0300

    feat: criando arquivo "router" no frontend

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,39 +1,13 @@
 import React from 'react';
-import { Route, Navigate, Routes } from 'react-router-dom';
 
 import Provider from './context';
-
-import Login from './pages/Common/Login';
-import Register from './pages/Common/Register';
-import Products from './pages/Customer/Products';
-import CustomerOrders from './pages/Customer/CustomerOrders';
-import CustomerOrderDetails from './pages/Customer/CustomerOrderDetails';
-import Checkout from './pages/Customer/Checkout';
-import Manage from './pages/Admin/Manage';
-import NotFound from './pages/Common/Not Found';
-
-import SellerOrders from './pages/Seller/SellerOrders';
+import Router from './Router';
 
 function App() {
   return (
     <Provider>
       <div className="App select-none">
-        <Routes>
-          <Route exact path="/" element={ <Navigate to="/login" replace /> } />
-          <Route path="/login" element={ <Login /> } />
-          <Route path="/register" element={ <Register /> } />
-          <Route path="/customer/products" element={ <Products /> } />
-          <Route path="/customer/orders/" element={ <CustomerOrders /> } />
-          <Route
-            path="/customer/orders/:id"
-            element={ <CustomerOrderDetails /> }
-          />
-          <Route path="/customer/checkout" element={ <Checkout /> } />
-          <Route path="/seller/orders" element={ <SellerOrders /> } />
-          <Route path="/seller/orders/:id" element={ <CustomerOrderDetails /> } />
-          <Route path="/admin/manage" element={ <Manage /> } />
-          <Route path="/*" element={ <NotFound /> } />
-        </Routes>
+        <Router />
       </div>
     </Provider>
   );
